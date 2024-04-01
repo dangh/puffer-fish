f84a6053f342f3c11c51b709747a82e87b7ed8e5
Dang <huynhminhdang@gmail.com>
2024-04-01 22:23:08 +0800

    end dot expansion with slash

--- functions/_puffer_fish_expand_dots.fish
+++ functions/_puffer_fish_expand_dots.fish
@@ -1,8 +1,10 @@
-function _puffer_fish_expand_dots -d 'expand ... to ../.. etc'
+function _puffer_fish_expand_dots -d 'expand .. to ../ and ../. to ../../ etc'
     set -l cmd (commandline --cut-at-cursor)
     set -l split (string split -- ' ' $cmd)
-    if string match --quiet --regex -- '^(\.\./)*\.\.$' $split[-1]
-        commandline --insert '/..'
+    if string match --quiet --regex -- '^\.$' $split[-1]
+        commandline --insert './'
+    else if string match --quiet --regex -- '^(\.\./)+$' $split[-1]
+        commandline --insert '../'
     else
         commandline --insert '.'
     end
